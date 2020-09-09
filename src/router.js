456ab0cb80a961b64fc2f97d0cc52d217026db70
goonxh <xh011234@163.com>
2020-09-09 10:49:16 +0800

    router

--- src/router.js
+++ src/router.js
@@ -13,12 +13,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "home" */ './views/home')
+      component: () => import(/* webpackChunkName: "home" */ '@/views/home')
     },
     {
       path: '/todolist',
       name: 'todolist',
-      component: () => import(/* webpackChunkName: 'todolist' */ './views/todolist')
+      component: () => import(/* webpackChunkName: 'todolist' */ '@/views/todolist')
     }
   ]
 })
